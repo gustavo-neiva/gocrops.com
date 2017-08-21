8623dce6d303137754aa52ea3b45da8b8b51284e
Johanna Wagner <johanna.wagner@whu.edu>
2017-08-21 18:54:28 -0300

    validations added for models - closes #1

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+  has_many :products
+
+  validates :name, presence: true, uniqueness: true
+  validates :unit, presence: true, inclusion: { in: [100 kg] }
 end
+
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
+  has_many :crops
+
+  validates :name, :category_id, presence: true, uniqueness: true
+  validates :amount, presence: true
 end
