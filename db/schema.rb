add822ecf1b01a327b02a93a9b033c397af1f48c
Johanna Wagner <johanna.wagner@whu.edu>
2017-08-22 17:56:09 -0300

    updated the schema, migrations and schema should NOT include subcat anymore, check before working on a new branch

--- db/schema.rb
+++ db/schema.rb
@@ -45,14 +45,6 @@ ActiveRecord::Schema.define(version: 20170822173213) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
-  create_table "subcategories", force: :cascade do |t|
-    t.string "name"
-    t.bigint "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_subcategories_on_category_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -88,5 +80,4 @@ ActiveRecord::Schema.define(version: 20170822173213) do
   add_foreign_key "crops", "products"
   add_foreign_key "crops", "users"
   add_foreign_key "products", "categories"
-  add_foreign_key "subcategories", "categories"
 end
