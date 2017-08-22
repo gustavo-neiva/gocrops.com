892dbb0d42e0d2c88be052b6497fcb96566af46e
Johanna Wagner <johanna.wagner@whu.edu>
2017-08-22 15:33:59 -0300

    seeds for categories and subcategories - closes #22

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating categories and subcategories"
+Category.destroy_all
+Subcategory.destroy_all
+
+categories = [ "Fruit products", "Vegetable products" ]
+
+subcategories = [[ "Apples", "Apricots", "Cherries", "Grapes", "Kiwis", "Lemons",
+                   "Melons", "Oranges", "Peaches", "Pears", "Strawberries",
+                   "Watermelons" ],
+                 [ "Asparagus", "Beans", "Cabbage", "Carrots", "Cauliflower",
+                   "Cucumber", "Eggplants", "Garlic", "Lettuce", "Mushrooms",
+                   "Onions", "Peppers", "Tomatoes Round"]]
+
+categories.each_with_index do |category, index|
+  category_new = Category.new(name: category, unit: "100 kg")
+  category_new.save!
+  subcategories[index].each do |subcategory|
+    subcategory_new = Subcategory.new(name: subcategory, category_id: category_new.id)
+  end
+end
+
+
+puts "Finished!"
