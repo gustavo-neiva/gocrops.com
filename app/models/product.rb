b73afe2ee139b656fa34c0c614fb762ec05fd233
Johanna Wagner <johanna.wagner@whu.edu>
2017-08-22 18:25:16 -0300

    added association in product model - closes #32

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :crops
+  has_many :price_informations
 
   validates :name, :category_id, presence: true, uniqueness: true
   validates :amount, presence: true
