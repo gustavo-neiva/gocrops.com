def7dcb43a1dc0f0e3c5a4a3750d4d46bd387bda
Daniel Ribeiro <asthedayislong@gmail.com>
2017-08-23 18:27:22 -0300

    Non-facebook avatar is working.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   namespace :profile do
-    resources :users
+    resource :users
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
