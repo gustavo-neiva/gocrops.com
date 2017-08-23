c6941ece3b03e397ecf4bb154d6e9e4c1c2a9f3f
Daniel Ribeiro <asthedayislong@gmail.com>
2017-08-23 10:39:35 -0300

    created different uploaders for Farm pictures and Crop pictures.

--- app/models/crop.rb
+++ app/models/crop.rb
@@ -5,6 +5,8 @@ class Crop < ApplicationRecord
   validates :user_id, uniqueness: { scope: :product_id }
   validate :validate_date_in_present
 
+  mount_uploader :pictures, CropsUploader
+
   def validate_date_in_present
     if harvest_date.present? && harvest_date < Date.today
       errors.add(:harvest_date, "Please choose a harvest date date that is in the present!")
