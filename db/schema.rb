a0da656b8a02b40bc1cb5d26aab3198942a95acf
Johanna Wagner <johanna.wagner@whu.edu>
2017-08-23 10:32:37 -0300

    Corrected the products table and the seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822230346) do
+ActiveRecord::Schema.define(version: 20170823132356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 20170822230346) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.bigint "category_id"
-    t.decimal "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "icon"
