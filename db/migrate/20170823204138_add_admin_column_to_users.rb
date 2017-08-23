b5b119153171dc7c0acdb0bf6ab30ea46115df95
Daniel Ribeiro <asthedayislong@gmail.com>
2017-08-23 15:31:52 -0300

    Admin Inteface implemented (Activeadmin)

--- db/migrate/20170823204138_add_admin_column_to_users.rb
+++ db/migrate/20170823204138_add_admin_column_to_users.rb
@@ -1,4 +1,4 @@
-class AddAdminColumnToUsers < ActiveRecord::Migration[5.1]
+class AddAdminColumn  ToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :admin, :boolean, null: false, default: false
   end
