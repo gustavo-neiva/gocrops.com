7082eb82ad2b27a4a777fb8eeca04395475b67c0
Ronalson Filho <contact@ronalson.com.br>
2017-08-24 16:59:20 -0300

    Implement dashboard/profile view linking to devise#edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :get_weather_data
+  before_action :active_navbar, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || stored_location_for(resource) || dashboard_path
@@ -15,6 +16,14 @@ class ApplicationController < ActionController::Base
     options = { units: "metric", APPID: ENV['OpenWeather'] }
     if session[:weather].nil? && current_user && current_user.farm_location
       session[:weather] = OpenWeather::Current.city(current_user.farm_location, options)
+    end
+  end
+
+  def active_navbar
+    # Checks if users is in devise#edit
+    # Add profile class params to active navbar
+    if params[:controller] == "devise/registrations" && params[:action] == "edit"
+      @profile_class = "navbar-dashboard-link__active"
     end
   end
 
