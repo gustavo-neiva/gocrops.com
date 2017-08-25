8e54529c0f9b782ce042cfa7cc617d54595737c4
Daniel Ribeiro <asthedayislong@gmail.com>
2017-08-25 15:04:56 -0300

    Added fields Address, Latitude, Longitude to table Users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823204139) do
+ActiveRecord::Schema.define(version: 20170825180344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,9 @@ ActiveRecord::Schema.define(version: 20170823204139) do
     t.string "avatar"
     t.string "farm_picture"
     t.boolean "admin", default: false, null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
