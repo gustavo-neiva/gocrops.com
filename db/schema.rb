56450f348bc3b14b34e4e5016a5dba190d7477bf
Luiz Neiva <luizgzn@gmail.com>
2017-08-28 12:38:32 -0300

    Removed subcategories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828145951) do
+ActiveRecord::Schema.define(version: 20170828153705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,14 +68,6 @@ ActiveRecord::Schema.define(version: 20170828145951) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
-  create_table "subcategories", force: :cascade do |t|
-    t.string "name"
-    t.bigint "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_subcategories_on_category_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -115,5 +107,4 @@ ActiveRecord::Schema.define(version: 20170828145951) do
   add_foreign_key "crops", "users"
   add_foreign_key "price_informations", "products"
   add_foreign_key "products", "categories"
-  add_foreign_key "subcategories", "categories"
 end
